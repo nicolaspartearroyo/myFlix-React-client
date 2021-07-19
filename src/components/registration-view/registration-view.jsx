57a1ac9e4d0fe039f2d869d0e236a0da2ea60bf4
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-07-19 21:08:44 +0200

    import Row fixed

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
 import { Link } from "react-router-dom";
 
 
